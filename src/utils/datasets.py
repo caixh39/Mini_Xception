623a82f395252cd042eb7ed141462ea9e0d73048
caixh <1127518237@qq.com>
2018-07-30 22:58:09 +0800

    data save function

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -140,3 +140,13 @@ def split_data(x, y, validation_split=.2):
     val_data = (val_x, val_y)
     return train_data, val_data
 
+
+def appendDFToCSV_void(df, csvFilePath, sep=","):    
+    if not os.path.isfile(csvFilePath):
+        df.to_csv(csvFilePath, mode='a', index=False, sep=sep)
+    elif len(df.columns) != len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns):
+        raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) + " columns. CSV file has " + str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns)) + " columns.")
+    elif not (df.columns == pd.read_csv(csvFilePath, nrows=1, sep=sep).columns).all():
+        raise Exception("Columns and column order of dataframe and csv file do not match!!")
+    else:
+        df.to_csv(csvFilePath, mode='a', index=False, sep=sep, header=False)
