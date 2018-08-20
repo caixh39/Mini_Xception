7ae04f39664936a9587696a05f229de7d5d11692
caixh <1127518237@qq.com>
2018-08-20 22:13:13 +0800

    fer2013 split into train and val dataset

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 from scipy.io import loadmat
 import pandas as pd
 import numpy as np
@@ -8,54 +11,33 @@ import cv2
 class DataManager(object):
     """Class for loading fer2013 emotion classification dataset or
         imdb gender classification dataset."""
-    def __init__(self, dataset_name='imdb', dataset_path=None, image_size=(48, 48)):
+    def __init__(self, dataset_mode=None, dataset_path=None, image_size=(48, 48)):
 
-        self.dataset_name = dataset_name
         self.dataset_path = dataset_path
         self.image_size = image_size
+        self.dataset_mode = dataset_mode
+
         if self.dataset_path != None:
             self.dataset_path = dataset_path
-        elif self.dataset_name == 'imdb':
-            self.dataset_path = '../datasets/imdb_crop/imdb.mat'
-        elif self.dataset_name == 'fer2013':
-            self.dataset_path = '../datasets/fer2013/fer2013.csv'
-        elif self.dataset_name == 'KDEF':
-            self.dataset_path = '../datasets/KDEF/'
+        elif self.dataset_mode == 'train':
+            self.dataset_path = '../datasets/fer2013/train.csv'
+        elif self.dataset_mode == 'val':   # fer2013 'PublicTest'
+            self.dataset_path = '../datasets/fer2013/val.csv'
+        elif self.dataset_mode == 'test':  # fer2013 'PrivateTest'
+            self.dataset_path = '../datasets/fer2013/test.csv'
         else:
-            raise Exception('Incorrect dataset name, please input imdb or fer2013')
+            raise Exception('Incorrect dataset name, please input fer2013 coresponding')
 
-    def get_data(self):
-        if self.dataset_name == 'imdb':
-            ground_truth_data = self._load_imdb()
-        elif self.dataset_name == 'fer2013':
-            ground_truth_data = self._load_fer2013()
-        elif self.dataset_name == 'KDEF':
-            ground_truth_data = self._load_KDEF()
-        return ground_truth_data
 
-    def _load_imdb(self):
-        face_score_treshold = 3
-        dataset = loadmat(self.dataset_path)
-        image_names_array = dataset['imdb']['full_path'][0, 0][0]
-        gender_classes = dataset['imdb']['gender'][0, 0][0]
-        face_score = dataset['imdb']['face_score'][0, 0][0]
-        second_face_score = dataset['imdb']['second_face_score'][0, 0][0]
-        face_score_mask = face_score > face_score_treshold
-        second_face_score_mask = np.isnan(second_face_score)
-        unknown_gender_mask = np.logical_not(np.isnan(gender_classes))
-        mask = np.logical_and(face_score_mask, second_face_score_mask)
-        mask = np.logical_and(mask, unknown_gender_mask)
-        image_names_array = image_names_array[mask]
-        gender_classes = gender_classes[mask].tolist()
-        image_names = []
-        for image_name_arg in range(image_names_array.shape[0]):
-            image_name = image_names_array[image_name_arg][0]
-            image_names.append(image_name)
-        return dict(zip(image_names, gender_classes))
 
-    def _load_fer2013(self):
+    # def get_data(self):
+    #     if self.dataset_mode == 'fer2013':
+    #         ground_truth_data = self._load_fer2013()
+    #     return ground_truth_data
+
+    def load_fer2013(self):
         data = pd.read_csv(self.dataset_path)
-        pixels = data['pixels'].tolist()
+        pixels = data['pixels'].tolist() # nested list of array elements. pixels is a list 
         width, height = 48, 48
         faces = []
         for pixel_sequence in pixels:
@@ -63,40 +45,14 @@ class DataManager(object):
             face = np.asarray(face).reshape(width, height)
             face = cv2.resize(face.astype('uint8'), self.image_size)
             faces.append(face.astype('float32'))
-        faces = np.asarray(faces)
-        faces = np.expand_dims(faces, -1)
+
+        faces = np.asarray(faces)          
+        faces = np.expand_dims(faces, -1) # inserting a new axis, -1 equal to 1 ?
         emotions = pd.get_dummies(data['emotion']).as_matrix()
+
         return faces, emotions
 
-    def _load_KDEF(self):
-        class_to_arg = get_class_to_arg(self.dataset_name)
-        num_classes = len(class_to_arg)
-
-        file_paths = []
-        for folder, subfolders, filenames in os.walk(self.dataset_path):
-            for filename in filenames:
-                if filename.lower().endswith(('.jpg')):
-                    file_paths.append(os.path.join(folder, filename))
-
-        num_faces = len(file_paths)
-        y_size, x_size = self.image_size
-        faces = np.zeros(shape=(num_faces, y_size, x_size))
-        emotions = np.zeros(shape=(num_faces, num_classes))
-        for file_arg, file_path in enumerate(file_paths):
-            image_array = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-            image_array = cv2.resize(image_array, (y_size, x_size))
-            faces[file_arg] = image_array
-            file_basename = os.path.basename(file_path)
-            file_emotion = file_basename[4:6]
-            # there are two file names in the dataset that don't match the given classes
-            try:
-                emotion_arg = class_to_arg[file_emotion]
-            except:
-                continue
-            emotions[file_arg, emotion_arg] = 1
-        faces = np.expand_dims(faces, -1)
-        return faces, emotions
-
+    
 def get_labels(dataset_name):
     if dataset_name == 'fer2013':
         return {0:'angry',1:'disgust',2:'fear',3:'happy',
@@ -145,7 +101,8 @@ def appendDFToCSV_void(df, csvFilePath, sep=","):
     if not os.path.isfile(csvFilePath):
         df.to_csv(csvFilePath, mode='a', index=False, sep=sep)
     elif len(df.columns) != len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns):
-        raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) + " columns. CSV file has " + str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns)) + " columns.")
+        raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) + 
+            " columns. CSV file has " + str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns)) + " columns.")
     elif not (df.columns == pd.read_csv(csvFilePath, nrows=1, sep=sep).columns).all():
         raise Exception("Columns and column order of dataframe and csv file do not match!!")
     else:
