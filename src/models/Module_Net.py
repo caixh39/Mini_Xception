7cf1f2c9f5f246e5adca2f6e24ef5cd505dad237
caixh <1127518237@qq.com>
2018-09-12 10:39:16 +0800

    MobileNet + XceptionNet

--- src/models/Module_Net.py
+++ src/models/Module_Net.py
@@ -1,9 +1,30 @@
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+import numpy as np
 from keras import backend as K
+from keras.preprocessing import image
+from keras import layers
+from keras.models import Model
+from keras.layers import Dense
+from keras.layers import multiply
+from keras.layers import Permute
+from keras.layers import Input
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import Reshape
+from keras.layers import BatchNormalization
 from keras.layers import GlobalAveragePooling2D
-from keras.layers import Reshape, Dense, multiply, Permute
+from keras.layers import GlobalMaxPooling2D
+from keras.layers import Conv2D
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras.regularizers import l2
+from keras.utils import conv_utils
+from keras.utils.data_utils import get_file
+from keras.engine.topology import get_source_inputs
+from keras.engine import InputSpec
 
 
 def squeeze_excite_block(input, ratio=16):
@@ -22,7 +43,7 @@ def squeeze_excite_block(input, ratio=16):
     se = Reshape(se_shape)(se)
     # kernel_initializer='he_normal', use_bias=False
     se = Dense(filters // ratio, activation='relu', kernel_initializer='he_uniform', use_bias=False)(se)
-    se = Dense(filters, activation='sigmoid', kernel_initializer='he_uniform', use_bias=False)(se)
+    se = Dense(filters, activation='sigmoid', kernel_initializer='glorot_uniform', use_bias=False)(se)
     
     if K.image_data_format() == 'channels_first':
         se = Permute((3, 1, 2))(se)
@@ -32,13 +53,9 @@ def squeeze_excite_block(input, ratio=16):
 
 
 
-def conv2d_bn(x,
-              filters,
-              num_row,
-              num_col,
-              padding='same',
-              strides=(1, 1),
-              name=None):
+def conv2d_bn(x,filters,num_row,num_col,padding='same',strides=(1, 1),
+            kernel_regularizer=None,
+            name=None):
     """Utility function to apply conv + BN.
     # Arguments
         x: input tensor.
@@ -60,18 +77,198 @@ def conv2d_bn(x,
         bn_name = None
         conv_name = None
 
-    if backend.image_data_format() == 'channels_first':
+    if K.image_data_format() == 'channels_first':
         bn_axis = 1
     else:
-        bn_axis = 3
-        
+        bn_axis = 3   
     x = layers.Conv2D(
         filters, (num_row, num_col),
         strides=strides,
         padding=padding,
         use_bias=False,
-        kernel_regularizer=regularization
+        kernel_regularizer=None,
         name=conv_name)(x)
     x = layers.BatchNormalization(axis=bn_axis, scale=False, name=bn_name)(x)
     x = layers.Activation('relu', name=name)(x)
+    return x
+
+class DepthwiseConv2D(Conv2D):
+    """Depthwise separable 2D convolution.
+
+    Depthwise Separable convolutions consists in performing
+    just the first step in a depthwise spatial convolution
+    (which acts on each input channel separately).
+
+    The `depth_multiplier` argument controls how many
+    output channels are generated per input channel in the depthwise step.
+
+    # Input shape: [batch, rows, cols, channels]` if data_format='channels_last'.
+    # Output shape: [batch, new_rows, new_cols, filters]` if data_format='channels_last'
+    """
+
+    def __init__(self,
+                 kernel_size,
+                 strides=(1, 1),
+                 padding='valid',
+                 depth_multiplier=1,
+                 data_format=None,
+                 activation=None,
+                 use_bias=True,
+                 depthwise_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 depthwise_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 depthwise_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        super(DepthwiseConv2D, self).__init__(
+            filters=None,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding=padding,
+            data_format=data_format,
+            activation=activation,
+            use_bias=use_bias,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            bias_constraint=bias_constraint,
+            **kwargs)
+
+        self.depth_multiplier = depth_multiplier
+        self.depthwise_initializer = initializers.get(depthwise_initializer)
+        self.depthwise_regularizer = regularizers.get(depthwise_regularizer)
+        self.depthwise_constraint = constraints.get(depthwise_constraint)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+    def build(self, input_shape):
+        # backend is tensorflow
+        channel_axis = 3
+        input_dim = int(input_shape[channel_axis])
+        depthwise_kernel_shape = (self.kernel_size[0],
+                                  self.kernel_size[1],
+                                  input_dim,
+                                  self.depth_multiplier)
+
+        self.depthwise_kernel = self.add_weight(
+            shape=depthwise_kernel_shape,
+            initializer=self.depthwise_initializer,
+            name='depthwise_kernel',
+            regularizer=self.depthwise_regularizer,
+            constraint=self.depthwise_constraint)
+
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(input_dim * self.depth_multiplier,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=4, axes={channel_axis: input_dim})
+        self.built = True
+
+    def call(self, inputs, training=None):
+        outputs = K.depthwise_conv2d(
+            inputs,
+            self.depthwise_kernel,
+            strides=self.strides,
+            padding=self.padding,
+            dilation_rate=self.dilation_rate,
+            data_format=self.data_format)
+
+        if self.bias:
+            outputs = K.bias_add(
+                outputs,
+                self.bias,
+                data_format=self.data_format)
+
+        if self.activation is not None:
+            return self.activation(outputs)
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_first':
+            rows = input_shape[2]
+            cols = input_shape[3]
+            out_filters = input_shape[1] * self.depth_multiplier
+        elif self.data_format == 'channels_last':
+            rows = input_shape[1]
+            cols = input_shape[2]
+            out_filters = input_shape[3] * self.depth_multiplier
+
+        rows = conv_utils.conv_output_length(rows, self.kernel_size[0],
+                                             self.padding,
+                                             self.strides[0])
+        cols = conv_utils.conv_output_length(cols, self.kernel_size[1],
+                                             self.padding,
+                                             self.strides[1])
+
+        if self.data_format == 'channels_first':
+            return (input_shape[0], out_filters, rows, cols)
+        elif self.data_format == 'channels_last':
+            return (input_shape[0], rows, cols, out_filters)
+
+    def get_config(self):
+        config = super(DepthwiseConv2D, self).get_config()
+        config.pop('filters')
+        config.pop('kernel_initializer')
+        config.pop('kernel_regularizer')
+        config.pop('kernel_constraint')
+        config['depth_multiplier'] = self.depth_multiplier
+        config['depthwise_initializer'] = initializers.serialize(self.depthwise_initializer)
+        config['depthwise_regularizer'] = regularizers.serialize(self.depthwise_regularizer)
+        config['depthwise_constraint'] = constraints.serialize(self.depthwise_constraint)
+        return config
+
+
+def conv_block(inputs, filters, alpha, kernel=(3, 3), strides=(1, 1)):
+    """Adds an initial convolution layer (with batch normalization and relu6).
+    # Input shape (samples, rows, cols, channels)` if data_format='channels_last'.
+    # Returns: Output tensor of block.
+    """
+    channel_axis = -1 
+    filters = int(filters * alpha)
+    x = Conv2D(filters, kernel, 
+                padding='same',
+                use_bias=False, 
+                strides=strides)(inputs)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = layers.Activation('relu')(x)
+    return x
+
+
+def depthwise_conv_block(inputs, pointwise_conv_filters, alpha,
+                          depth_multiplier=1, strides=(1, 1), block_id=1):
+    """Adds a depthwise convolution block.
+       A depthwise convolution block consists of a depthwise conv,
+       batch normalization, relu6, pointwise convolution,
+       batch normalization and relu6 activation.
+
+    # Input shape 4D tensor with shape:
+      (batch, rows, cols, channels) if data_format='channels_last'
+    # Returns
+        Output tensor of block.  """
+
+    channel_axis = -1
+    pointwise_conv_filters = int(pointwise_conv_filters * alpha)
+
+    x = DepthwiseConv2D((3, 3),
+                        padding='same',
+                        depth_multiplier=depth_multiplier,
+                        strides=strides,
+                        use_bias=False,
+                        name='conv_dw_%d' % block_id)(inputs)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = layers.Activation('relu')(x)
+
+    x = Conv2D(pointwise_conv_filters, (1, 1),
+               padding='same',
+               use_bias=False,
+               strides=(1, 1),
+               name='conv_pw_%d' % block_id)(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = layers.Activation('relu')(x)
     return x
